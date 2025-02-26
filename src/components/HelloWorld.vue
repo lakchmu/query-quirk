5d4c0f479a39e2c97ce157289b61f5673ec97157
Daria Kurmaeva <daria.kurmaeva@gmail.com>
2025-02-26 09:40:47 +0600

    Init commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      This project is a collection of mini-apps, each of which solves problems that will help you
+      not only shine at an interview, but also master a bunch of new features and technologies
+      before you forget how to write JavaScript!
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/countries',
+      name: 'countries',
+      // route level code-splitting
+      // this generates a separate chunk (Countries.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/CountriesView.vue'),
+    },
+  ],
+})
+
+export default router
